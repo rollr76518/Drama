fcf0db45d8c867f38dd27ade188aee0d16d00c9f
Hanyu <rollr76518@gmail.com>
2018-11-03 19:41:10 +0800

    viewControllers structure

---
+++ Drama/DramaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DramaViewController.swift
+//  Drama
+//
+//  Created by Ryan on 2018/11/3.
+//  Copyright © 2018年 Hanyu. All rights reserved.
+//
+
+import UIKit
+
+class DramaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
